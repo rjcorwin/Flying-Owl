300f28b19bfc15cda40153deea73ae76ded54810
rjsteinert <rj@rjsteinert.com>
2013-03-01 11:01:21 -0500

    Initial scaffolding

---
+++ programs/portable-library/portable-library.js
@@ -0,0 +1,32 @@
+
+$.portableLibrary = {}
+
+$.portableLibrary.init = function() {
+
+  // Import the user's score card 
+
+  // Import the content database
+
+  // Show the content database
+
+}
+
+$.portableLibrary.scoreCard = function() {
+
+  var data
+
+  this.create = function() {
+
+  }
+
+  this.read = function() {
+
+  }
+
+  this.update = function () {
+
+
+  }
+
+}
+
