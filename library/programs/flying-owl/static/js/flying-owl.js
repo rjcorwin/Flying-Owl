60418c6756926d066e94f562f573f0f6daa7fd85
rjsteinert <rj@rjsteinert.com>
2013-03-03 00:50:43 +0000

    Topic list exploration now working.

--- library/programs/flying-owl/static/js/flying-owl.js
+++ library/programs/flying-owl/static/js/flying-owl.js
@@ -1,46 +1,82 @@
 (function ($) {
 
-  $.flyingOWL = {}
+  $.flyingOwl = {}
 
-  $.flyingOWL.init = function() {
+  /*
+   * By the time this loads, everyting from the file system is in localStorage
+   */
+  $.flyingOwl.bootstrap = function() {
 
     // The library has been loaded. Move on to the topic list.
     $("#message").html(
-      "Your library has been loaded. <a href='programs/flying-owl/templates/topic-list.html'>Click here to see the topic list.</a>"
+      "Your library has been loaded. <a href='programs/flying-Owl/templates/topic-list.html'>Click here to see the topic list.</a>"
     )
     
 
   }
 
-  $.flyingOWL.library = {
+  $.flyingOwl.init = function() {
+    $.flyingOwl.topics.init()
+  }
+
+  $.flyingOwl.topics = {
 
     data: {},
 
     init: function () {
       this.read()
-      this.refreshView()
     },
 
     read: function() {
-      if (dataCallback.data['library']) {  
-        this.data = dataCallback.data['library']
-      }
-      else {
+      this.data = JSON.parse(localStorage.getItem('flyingOwlTopics'))
+      if (!this.data) {  
         alert("We could not find the content for your Library :-(.  Is it missing?")
       }
-
+      console.log(this.data)
     },
 
-    refreshView: function() {
-      console.log(this.data)
-      $.each(this.data.content, function(key, resource) {
+    refreshView: function(path) {
 
+      // Get the children, start at the beginning
+      var currentTopic = this.data
+      if(path != null && path != "/") {
+        // build a walkway
+        var pathArray = path.split("/")
+        for(var i=0, l=pathArray.length; i<l; i++) {
+          if(i==0) {
+            pathArray[i] = "/"
+          }
+          else {
+            pathArray[i] = pathArray[i-1] + pathArray[i] + "/"
+          }
+        }
+        // The last entry doesn't make sense, drop it
+        pathArray.pop()
+        // walk up the the path redefining currentTopic as we go
+        var currentPath = ""
+        for(var i=0, l=pathArray.length; i<l; i++) {
+          if(i != 0) { 
+            for(var c=0, len=currentTopic.children.length; c<len; c++) {
+              if(currentTopic.children[c].path == pathArray[i]) {
+                currentTopic = currentTopic.children[c]
+                break
+              }  
+            }
+          }
+        }
+      }
+
+      console.log(currentTopic)
+      var topicTemplate = Handlebars.compile($("#topic-template").html())
+      $.each(currentTopic.children, function(key, topic) {
+        // Print the path and title
+        $("body").append(topicTemplate(topic))
       })
     }
 
   }
 
-  $.flyingOWL.libraryCard = {
+  $.flyingOwl.libraryCard = {
 
     data: {},
 
